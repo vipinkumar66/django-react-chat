c453306fe174441b09f65fc1a3fe0166c10f36e3
vipin <vipinkumarlamba55@gmail.com>
2023-08-06 00:17:09 +0530

    Server app models created: Category, server, channel

--- djchat/server/admin.py
+++ djchat/server/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Category, Server, Channel
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Server)
+admin.site.register(Channel)
+
--- djchat/server/models.py
+++ djchat/server/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Server(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,
+                              related_name="server_owner")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,
+                                 related_name="server_catgory")
+    description = models.CharField(max_length=250, null=True)
+    members = models.ManyToManyField(settings.AUTH_USER_MODEL)
+
+
+    def __str__(self):
+        return self.name
+
+
+class Channel(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+                              related_name="channel_owner")
+    server = models.ForeignKey(Server, on_delete=models.CASCADE,
+                               related_name="channel_server")
+    topic = models.CharField(max_length=100)
+
+    def save(self, *args, **kwargs):
+        """
+        Here we are just doing some modification to the name
+        field of the model before saving it and making it all
+        lowercase
+        """
+        self.name = self.name.lower()
+        super(Channel, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
