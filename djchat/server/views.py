f0503bc8c6515aab871e4a0fae6a5874161b5cab
vipin <vipinkumarlamba55@gmail.com>
2023-08-07 08:39:27 +0530

    get servers on basis of quantity

--- djchat/server/views.py
+++ djchat/server/views.py
@@ -9,9 +9,12 @@ class ServerListViewSet(viewsets.ViewSet):
 
     def list(self, request):
         category = request.query_params.get("category")
-        print("category",category)
+        qty = request.query_params.get("qty")
         if category:
             self.queryset = self.queryset.filter(category__name = category)
+
+        if qty and type(qty)==int:
+            self.queryset = self.queryset[:int(qty)]
 
         serializer = ServerSerializer(self.queryset, many=True)
         return Response(serializer.data)
