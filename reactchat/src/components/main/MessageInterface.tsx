3c52118419e4914bd35d83b183b47fe2e4420212
vipin <vipinkumarlamba55@gmail.com>
2023-08-20 10:09:26 +0530

    server Landing page completed

--- reactchat/src/components/main/MessageInterface.tsx
+++ reactchat/src/components/main/MessageInterface.tsx
@@ -3,6 +3,12 @@ import { useParams } from "react-router-dom"
 import useWebSocket from "react-use-websocket"
 import useCrud from "../../hooks/useCrud"
 import { Server } from "../../@types/server"
+import { Box, Typography } from "@mui/material"
+
+
+interface ServerChannelProps{
+    data: Server[];
+}
 
 interface Message{
     sender: string;
@@ -10,11 +16,14 @@ interface Message{
     timestamp: string;
 }
 
-const MessageInterface = () => {
+const MessageInterface = (props:ServerChannelProps) => {
 
+    const {data} = props;
     const [message, setMessage] = React.useState("")
     const [newMessage, setNewMessage] = React.useState<Message[]>([]);
     const {serverId, channelId} = useParams();
+
+    const server_name = data?.[0]?.name ?? "Server";
 
     const { fetchData } = useCrud<Server>([], `/messages/?channel_id=${channelId}`)
 
@@ -47,29 +56,53 @@ const MessageInterface = () => {
 
   return (
     <>
-        <div>
-            {newMessage.map((msg:Message, index:number)=>{
-                return (
-                    <div key={index}>
-                        <p>{msg.sender}</p>
-                        <p>{msg.content}</p>
-                    </div>
-                );
-            })}
-        </div>
-        <form>
-            <label>
-                Enter Message:
-                <input
-                type="text"
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                />
-            </label>
-        </form>
-        <button onClick={() => {sendJsonMessage({type:"message", message})}}>
-            SEND MESSAGE
-        </button>
+    {channelId === undefined ? (<Box
+    sx={{
+        overflow:"hidden",
+        p:{xs:0},
+        height:`calc(80vh)`,
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center"
+    }}>
+        <Box sx={{textAlign:"center"}}>
+            <Typography variant="h4"
+            fontweight={700}
+            letterSpacing = {"-0.5px"}
+            sx={{px:5, maxWidth:"600px"}}>
+                Welcome to {server_name}
+            </Typography>
+            <Typography>{data?.[0]?.description ?? "This is our home"}</Typography>
+
+        </Box>
+    </Box>):
+    (
+        <>
+            <div>
+                {newMessage.map((msg:Message, index:number)=>{
+                    return (
+                        <div key={index}>
+                            <p>{msg.sender}</p>
+                            <p>{msg.content}</p>
+                        </div>
+                    );
+                })}
+            </div>
+            <form>
+                <label>
+                    Enter Message:
+                    <input
+                    type="text"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                    />
+                </label>
+            </form>
+            <button onClick={() => {sendJsonMessage({type:"message", message})}}>
+                SEND MESSAGE
+            </button>
+        </>
+    )}
     </>
   )
 }
--- reactchat/src/pages/Servers.tsx
+++ reactchat/src/pages/Servers.tsx
@@ -58,7 +58,7 @@ const Servers = () => {
         <ServerChannels data={dataCRUD}/>
       </SecondaryDraw>
       <Main>
-        <MessageInterface/>
+        <MessageInterface data={dataCRUD}/>
       </Main>
     </Box>
   )
